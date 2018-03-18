bcf41764ee007f0afeef15e4fc075da2d22f9456
Charles Zhang <charles@Charless-MacBook-Pro-2.local>
2018-03-18 18:46:37 -0400

    added second object ai1, added time tick, did some testing with the new stuff

--- joust.elm
+++ joust.elm
@@ -6,7 +6,7 @@ import Platform.Sub as Sub
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Keyboard as Key
-
+import Time exposing (Time)
 
 {- Main -}
 main : Program Never Game Msg
@@ -19,12 +19,14 @@ main = Html.program
 type alias Game = {
         position : Movers,
         ai1 : Movers
+
                    }
 type alias Movers = { x : Int, y : Int }
 
 --type Msg = KeyMsg Key.KeyCode
 type Msg
     = KeyMsg Key.KeyCode
+    | Tick Time
     | Nothing
 
 init = ({ position = {x = 300, y = 300}, ai1 = {x = 100, y = 100}})
@@ -36,6 +38,9 @@ update msg model = case msg of
 
       Nothing -> (model, Cmd.none)
 
+      Tick time ->
+          updateGame model
+
 movePos : Int -> Game -> (Game, Cmd.Cmd Msg)
 movePos keyCode model =
     case keyCode of
@@ -45,21 +50,32 @@ movePos keyCode model =
       68 -> ({ model | position = { x = model.position.x + 20, y = model.position.y } }, Cmd.none)
       _ -> (model, Cmd.none)
 
+updateGame : Game -> ( Game, Cmd Msg )
+updateGame model =
 
-
---updatePos : Game -> (Game, Cmd.Cmd)
-
-
-
+      ai1Pos model
+ai1Pos : Game -> ( Game, Cmd Msg )
+ai1Pos model =
+      if model.ai1.x < 400 then
+        ({ model | ai1 = { x = model.ai1.x + 10, y = model.ai1.y}, position = { x = model.ai1.x, y = model.ai1.y + 10}}, Cmd.none)
+      else
+        ({ model | ai1 = { x = model.ai1.x - 10, y = model.ai1.y}}, Cmd.none)
 
 view : Game -> Html.Html Msg
 view model = let
       posX = toString model.position.x
       posY = toString model.position.y
+      ai1X = toString model.ai1.x
+      ai1Y = toString model.ai1.y
     in svg [width "100%",height "100%"]
     (
        [rect [x posX,y posY, width "50", height "50", fill "red"] []]
-       ++ [rect [x posX,y posY, width "50", height "50", fill "red"] []])
+       ++ [rect [x ai1X,y ai1Y, width "50", height "50", fill "blue"] []])
 
 subscriptions : Game -> Sub Msg
-subscriptions model = Key.downs KeyMsg
+subscriptions model =
+    Sub.batch [Key.downs KeyMsg, tick]
+
+tick : Sub Msg
+tick =
+    Time.every (100 * Time.millisecond) Tick
